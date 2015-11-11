045c6d7e9b71438b818678e51f753f6fe92bd519
pf15ese <patrick.frischknecht@students.unibe.ch>
2015-11-11 12:37:20 +0100

    Readded TutorFormServiceTests

--- tutoris_bernae/src/test/java/org/sample/test/suites/ServiceTestSuite.java
+++ tutoris_bernae/src/test/java/org/sample/test/suites/ServiceTestSuite.java
@@ -5,9 +5,12 @@ import org.sample.test.service.EditFormServiceTest;
 import org.sample.test.service.EditFormServiceTransactionTest;
 import org.sample.test.service.RegisterFormServiceTest;
 import org.sample.test.service.RegisterFormServiceTransactionTest;
+import org.sample.test.service.TutorFormServiceTest;
+import org.sample.test.service.TutorFormServiceTransactionTest;
 import org.junit.runner.RunWith;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({EditFormServiceTest.class, EditFormServiceTransactionTest.class,
-					RegisterFormServiceTest.class, RegisterFormServiceTransactionTest.class})
+					RegisterFormServiceTest.class, RegisterFormServiceTransactionTest.class,
+					TutorFormServiceTest.class, TutorFormServiceTransactionTest.class})
 public class ServiceTestSuite {}

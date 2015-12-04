368d3c1f330c4379bc474be0e9bd1ba25a206a8c
gcorsini <guillaume.corsini@students.unibe.ch>
2015-12-04 14:03:52 +0100

    paypal trial

---
+++ tutoris_bernae/src/main/java/org/sample/general/Constants.java
@@ -0,0 +1,9 @@
+package org.sample.general;
+
+public class Constants {
+	public final static String TUTORSHIP_FEE = "10.00";
+	public final static String PAYPAL_SELLER_ID = "guillaume.corsini_api1.students.unibe.ch";//"jb-us-seller@paypal.com";
+	public final static String PAYPAL_SELLER_PW = "GX5TB3RJDYKV7VEN";
+	public final static String PAYPAL_SELLER_TOKEN = "A-XIducnvYJlGFTW6kREHKzGtSQQArvSvboQ6wC3tFjHo-N2ySZnj1TM";
+	
+}

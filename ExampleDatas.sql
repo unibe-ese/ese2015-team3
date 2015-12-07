011a44652cd6716b5743c2e51df8082291728a64
pf15ese <patrick.frischknecht@students.unibe.ch>
2015-12-07 09:29:21 +0100

    added example datas

---
+++ ExampleDatas.sql
@@ -0,0 +1,8 @@
+INSERT INTO studycourse(faculty,name) VALUES ('','Mathematics');
+INSERT INTO studycourse(faculty,name) VALUES ('','Computer Science');
+INSERT INTO classes(name) VALUES ('Programming 1');
+INSERT INTO classes(name) VALUES ('Programming 2');
+INSERT INTO classes(name) VALUES ('Introduction to Software Engineering');
+INSERT INTO classes(name) VALUES ('Human Computer Interfaces');
+INSERT INTO classes(name) VALUES ('Automata and formal languages');
+INSERT INTO classes(name) VALUES ('Computability and Complexity');

e9ba910dc2c49f8dbf07edd662ef2e6d178145d2
gcorsini <guillaume.corsini@students.unibe.ch>
2015-12-08 01:09:55 +0100

    added serializable to user and completedClasses

--- tutoris_bernae/src/main/java/org/sample/model/User.java
+++ tutoris_bernae/src/main/java/org/sample/model/User.java
@@ -1,5 +1,7 @@
 package org.sample.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -18,7 +20,9 @@ import javax.persistence.OneToOne;
  *
  */
 @Entity
-public class User {
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue
